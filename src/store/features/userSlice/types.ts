f1fc706b2b4166a6f6de237f2937588e21af2858
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-02-21 20:33:39 +0100

    Create userSlice

---
+++ src/store/features/userSlice/types.ts
@@ -0,0 +1,9 @@
+export interface UserState {
+  isLogged: boolean;
+}
+
+export interface User {
+  username: string;
+  token: string;
+  id: string;
+}
---
+++ src/store/features/userSlice/userSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserState, User } from "./types";
+
+const initialState: UserState = {
+  name: "",
+  password: "",
+  isLogged: false,
+  token: "",
+  id: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (initialState, action: PayloadAction<User>) => ({
+      ...action.payload,
+      isLogged: true,
+    }),
+  },
+});
+
+export const userReducer = userSlice.reducer;
+
+export const { loginUser: loginUserActionCreator } = userSlice.actions;
